593f794656b8e179c19852a39fb174eea9c726f5
Mario Kirven <mariokirven.mk@gmail.com>
2025-01-22 12:23:13 -0500

    Implement movement for rook subclass

--- lib/chess_board/models/rook.dart
+++ lib/chess_board/models/rook.dart
@@ -17,6 +17,92 @@ class Rook extends ChessPiece {
   (List<List<SquareData>>, List<Coordinate>) generatePossibleMoves(
     List<List<SquareData>> cleanBoard,
   ) {
-    return ([[]], []);
+    final posibleMoves = <Coordinate>[];
+    final posibleSquares = <SquareData>[];
+
+    //Looking north
+    int rankIndex = currentPosition.rank;
+    bool isOccupied = false;
+    while (rankIndex > 0 && !isOccupied) {
+      rankIndex -= 1;
+      final nextSquare = cleanBoard[rankIndex][currentPosition.file];
+      if (nextSquare.piece != null) {
+        isOccupied = true;
+        if (nextSquare.piece?.color != color) {
+          posibleSquares.add(nextSquare);
+          posibleMoves.add(nextSquare.coordinate);
+        }
+        continue;
+      }
+
+      posibleSquares.add(nextSquare);
+      posibleMoves.add(nextSquare.coordinate);
+    }
+
+    //Looking South
+    rankIndex = currentPosition.rank;
+    isOccupied = false;
+    while (rankIndex < 7 && !isOccupied) {
+      rankIndex += 1;
+      final nextSquare = cleanBoard[rankIndex][currentPosition.file];
+      if (nextSquare.piece != null) {
+        isOccupied = true;
+        if (nextSquare.piece?.color != color) {
+          posibleSquares.add(nextSquare);
+          posibleMoves.add(nextSquare.coordinate);
+        }
+        continue;
+      }
+
+      posibleSquares.add(nextSquare);
+      posibleMoves.add(nextSquare.coordinate);
+    }
+
+    //Looking west
+    int fileIndex = currentPosition.file;
+    isOccupied = false;
+    while (fileIndex > 0 && !isOccupied) {
+      fileIndex -= 1;
+      final nextSquare = cleanBoard[currentPosition.rank][fileIndex];
+      if (nextSquare.piece != null) {
+        isOccupied = true;
+        if (nextSquare.piece?.color != color) {
+          posibleSquares.add(nextSquare);
+          posibleMoves.add(nextSquare.coordinate);
+        }
+        continue;
+      }
+
+      posibleSquares.add(nextSquare);
+      posibleMoves.add(nextSquare.coordinate);
+    }
+
+    //Looking east
+    fileIndex = currentPosition.file;
+    isOccupied = false;
+    while (fileIndex < 7 && !isOccupied) {
+      fileIndex += 1;
+      final nextSquare = cleanBoard[currentPosition.rank][fileIndex];
+      if (nextSquare.piece != null) {
+        isOccupied = true;
+        if (nextSquare.piece?.color != color) {
+          posibleSquares.add(nextSquare);
+          posibleMoves.add(nextSquare.coordinate);
+        }
+        continue;
+      }
+
+      posibleSquares.add(nextSquare);
+      posibleMoves.add(nextSquare.coordinate);
+    }
+    //Highlight all possible moves
+    for (final move in posibleMoves) {
+      cleanBoard[move.rank][move.file] = SquareData(
+        isHighLighted: true,
+        cleanBoard[move.rank][move.file].piece,
+        coordinate: cleanBoard[move.rank][move.file].coordinate,
+      );
+    }
+    return (cleanBoard, posibleMoves);
   }
 }
