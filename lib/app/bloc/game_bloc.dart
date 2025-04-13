d281b4d9bd1a03d24a39f71dfa9093018e7463df
Mario Kirven <mariokirven.mk@gmail.com>
2025-04-13 13:54:53 -0500

    Handle movePiece exceptions

--- lib/app/bloc/game_bloc.dart
+++ lib/app/bloc/game_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:chess_shared/chess_shared.dart';
 import 'package:core_chess/core_chess.dart';
 import 'package:equatable/equatable.dart';
 
@@ -8,7 +9,10 @@ part 'game_state.dart';
 class GameBloc extends Bloc<GameEvent, GameState> {
   GameBloc()
       : super(
-          const GameState(fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0'),
+          const GameState(
+            fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0',
+            gameStatus: GameStatus.playing,
+          ),
         ) {
     on<ChessPieceMoved>(_onChessPieceMoved);
     on<ChessPieceSelected>(onChessPieceSelected);
@@ -16,13 +20,24 @@ class GameBloc extends Bloc<GameEvent, GameState> {
   final CoreChess _coreChess = CoreChess();
 
   void _onChessPieceMoved(ChessPieceMoved event, Emitter<GameState> emit) {
-    print('*Handler* Moving piece: ${event.lanMove}');
-    final newFen = _coreChess.makeMove(state.fen, event.lanMove);
-    emit(state.copyWith(fen: newFen, possibleMoves: []));
+    try {
+      final gameData = _coreChess.makeMove(state.fen, event.lanMove);
+
+      emit(
+        state.copyWith(
+          fen: gameData.fen,
+          possibleMoves: [],
+          gameStatus: gameData.status,
+        ),
+      );
+    } catch (exception) {
+      print('Error: $exception');
+      return;
+    }
   }
 
   void onChessPieceSelected(ChessPieceSelected event, Emitter<GameState> emit) {
-    final moves = _coreChess.getLegalMoves(state.fen, event.anSquare);
+    final moves = _coreChess.getLegalMovesForPiece(state.fen, event.anSquare);
     print('*Handler* Calculated moves: $moves');
     emit(state.copyWith(possibleMoves: moves));
   }
